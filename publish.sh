bf20475826df0c4fee0bb10d52602882a7bd006a
Serghei Filippov <blastowind@gmail.com>
2014-05-03 22:23:41 -0400

    Finalized publish script

--- publish.sh
+++ publish.sh
@@ -1,7 +1,16 @@
-#!/bin/bash -e
+#!/bin/bash
+
+URL='git@github.com:Saevon/saevon.github.io.git'
+KEEP='assets'
 
 branch=`git rev-parse --abbrev-ref HEAD`
-output='./.site'
+uuid=`uuidgen`
+
+cur=`pwd`
+tmp_prefix="blog-pelican"
+tmp="/tmp/$tmp_prefix-$uuid"
+output="$cur/output"
+
 
 # Always use the source branch to publish
 if [[ $branch != 'source' ]]; then
@@ -10,30 +19,47 @@ fi
 
 # Create the static site
 pelican -s publishconf.py -o $output
+mkdir -p $tmp
+
+cd $tmp
+if [[ `pwd` != $tmp ]]; then
+	echo 'ERROR: Not working in the correct directory'
+fi
+
+git init
+git remote add -f origin $URL
 
 # Get the latest version of master
 git checkout master
 if [[ $? != 0 ]]; then
+	echo "ERROR: Can't checkout master"
 	exit 1;
 fi
 
 # Remove all generated files
-git rm -r -q !(assets)
+find . -maxdepth 1 \! \( -name ".*" -or -name $KEEP \) | xargs git rm -r
 
-# Get all the generated files from the source branch
-git checkout source -- .publish-copy
+# Copy over the output content
+cp -R $output/ $tmp
+
+# Get all the extra files files from the source branch
+cp $cur/.publish-copy .
 while read file; do
-	git checkout source -- $file
+	cp $cur/content/$file .
+	if [[ $? != 0 ]]; then
+		echo "WARNING: File not found $file"
+	fi
 done < .publish-copy
 
+
 # Move the newly generated files to the directory
-cp -R $tmp/$output/ .
+cp -R $output/ $tmp
+rm -rf $cur/$output
 git add *
 
 # Update master
 git commit -a -m "Publishes new static content `date`"
 git push -f origin master
 
-# Clean up
-git checkout $branch
-rm -rf $tmp
+# Cleanup
+rm -rf /tmp/$tmp_prefix*
