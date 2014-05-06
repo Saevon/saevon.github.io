504731a50bc141b2b3b42a65db9692eabfa6358e
Serghei Filippov <blastowind@gmail.com>
2014-05-06 00:46:36 -0400

    Updates sidenav, active category for articles.
     * If you're on an article you now have the category selected in the sidenav.

--- plugins/jinja2/nav.py
+++ plugins/jinja2/nav.py
@@ -7,6 +7,12 @@ from jinja2 import contextfilter
 @contextfilter
 def is_cur_page(context, url, or_child=False):
     page = context.get('page_name')
+
+    # Articles use a different API so check those as well
+    article = context.get('article')
+    if article is not None:
+        page = article.url
+
     if or_child:
         return url in unicode(page)
     else:
