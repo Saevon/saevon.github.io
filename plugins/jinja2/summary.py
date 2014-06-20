f72995174e0a09bc85cc48da856c690058a2e461
Serghei Filippov <blastowind@gmail.com>
2014-06-20 03:20:06 -0400

    Adds to the summary extension (to reflect a new markdown extension)

--- plugins/jinja2/summary.py
+++ plugins/jinja2/summary.py
@@ -5,7 +5,8 @@ from __future__ import unicode_literals
 import re
 
 SUMMARY_RE = re.compile(
-    r'^(?P<content>.*?)(?P<headertag><h[1-6].*?>)',
+    # r'^(?P<content>.*?)(?P<headertag><h[1-6].*?>)',
+    r'^(?P<content>.*?)(?P<section><[^<>]*?class="[^"]*?section)',
     re.DOTALL,
 )
 
